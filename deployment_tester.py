5c57c58dec20d68f93e564872da5a0b92bbc2d08
ginger <gayansilva262@gmail.com>
2023-12-13 05:38:42 +0000

    mid point

--- deployment_tester.py
+++ deployment_tester.py
@@ -6,11 +6,12 @@ import threading
 import sys
 import requests
 
-REQUEST_COUNT = 95
+REQUEST_COUNT = 1
 DELAY = 0.01
 SERVER = "istio"
 SERVER = "mine"
 ip = "35.233.179.102:80" if SERVER == "istio" else "34.82.114.109:5000"
+ip = "35.233.179.102:80" if SERVER == "istio" else "10.108.0.68:5000"
 log_file = "logs.log" if SERVER == "istio" else "logs_mine.log"
 logging.basicConfig(filename=log_file, filemode='a', format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 logging.info(f"Started Application test using DELAY of {DELAY} and {REQUEST_COUNT} of requests")
@@ -33,13 +34,23 @@ def synthesize(text, callid, index):
         response = requests.post(url, headers=headers, data=json.dumps(data), stream=True)
         if response.status_code == 200:
             start = True
+            file_start = True
             string = ""
+            file = open(f'result/{index}.wav', 'wb')
+            header = open(f"bark/assets/header.raw", 'rb').read()
+            file.write(header)
             for chunk in response.iter_content(chunk_size=None):
-                string += chunk.decode('ascii')
-                logging.info(string)
                 if start:
                     logging.info(f"Received: {index} {callid} {time.time() - s}")
                     start = False
+                    string += chunk.decode('ascii')
+                    logging.info(string)
+                else:
+                    if file_start:
+                        logging.info(f"First Chunk Received {index} {callid} {time.time() - s}")
+                        file_start = False
+                    file.write(chunk)
+            file.close()
             logging.info(f"Finished: {index} {time.time() - s} {string}")
             done = True
         elif response.status_code == 400:
--- docker_build.sh
+++ docker_build.sh
@@ -1,4 +1,4 @@
-export TAG=1.1.test.1205
+export TAG=1.1.test.1211
 docker build -t fastapi-tts:$TAG .
 docker tag fastapi-tts:$TAG gcr.io/air-pulumi/tts:$TAG
 docker push gcr.io/air-pulumi/tts:$TAG
