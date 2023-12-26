9a3249357bb3ef0e9882a9bebdde368c93a1c52e
ginger <gayansilva262@gmail.com>
2023-12-26 14:38:20 +0000

    deploy 12.25 added voice download functionality fixed npz path error added liveness prob

--- docker_build.sh
+++ docker_build.sh
@@ -1,4 +1,4 @@
-export TAG=1.1.test.1213
+export TAG=1.1.test.1225
 docker build -t fastapi-tts:$TAG .
 docker tag fastapi-tts:$TAG gcr.io/air-pulumi/tts:$TAG
 docker push gcr.io/air-pulumi/tts:$TAG
--- fast_api_server.py
+++ fast_api_server.py
@@ -8,6 +8,7 @@ import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.responses import StreamingResponse, Response
 from bark.SynthesizeThread import SynthesizeThread
+from bucket_utils import download_voice
 
 DEFAULT_VOICE = 'test'
 CALL_INDEX = 0
@@ -31,37 +32,50 @@ root.addHandler(ch)
 app = FastAPI()
 @app.post('/{call_id}/synthesize')
 async def synthesize(call_id: str, request: Request):
-    if synthesize_thread.is_busy():
-        def stream_results():
-            yield os.getenv('MY_POD_NAME')
-        return StreamingResponse(stream_results(), status_code=400)
+    # if synthesize_thread.is_busy():
+    #     def stream_results():
+    #         yield os.getenv('MY_POD_NAME')
+    #     return StreamingResponse(stream_results(), status_code=400)
     is_process_available = False
     global CALL_INDEX
     # call_id = "CA123" if port == 5000 else "CA124"
     call_id = f"CA{CALL_INDEX}"
     data = await request.json()
     text = data.pop("text")
-    voice = data.pop("voice")
+    voice = data.pop("voice").replace('.npz', '')
     rate = data.pop("rate") if "rate" in data.keys() else 1.0
-    rate -= 0.1
+
+    if voice + ".npz" not in os.listdir("bark/assets/prompts"):
+        result = download_voice('tts-voices-npz', voice, 'bark/assets/prompts')
+        if not result:
+            def stream_results():
+                yield f"NO VOICE {voice}"
+            return StreamingResponse(stream_results(), status_code=400)
+
     stream = synthesize_thread.add_request(text, voice, rate)
     async def stream_results():
         yield os.getenv('MY_POD_NAME')
         async for out in stream:
             yield out
-            # yield os.getenv('MY_POD_NAME')
     return StreamingResponse(stream_results(), media_type="application/octet-stream")
 
 
 @app.get('/process_available')
 def get_readiness():
     if not synthesize_thread.is_busy():
-        print("Ready", os.getenv('MY_POD_NAME'))
         return Response(status_code=200)
     else:
         print("Busy", os.getenv('MY_POD_NAME'))
         return Response(status_code=401)
 
+
+@app.get('/is_alive')
+def get_liveness():
+    if synthesize_thread.is_alive():
+        return Response(status_code=200)
+    else:
+        print("Server is Dead", os.getenv('MY_POD_NAME'))
+        return Response(status_code=401)
 
 # launch a Tornado server with HTTPServer.
 if __name__ == "__main__":
