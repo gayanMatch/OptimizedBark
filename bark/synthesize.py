08bc33fc415f25411a75180156c63385a2feb46d
gayanMatch <gayansilva262@gmail.com>
2024-02-09 04:50:31 +0000

    fixed sentence splitting

--- bark/synthesize.py
+++ bark/synthesize.py
@@ -15,7 +15,7 @@ def synthesize(text="", stream=None, voice="en_fiery", rate=1.0):
     last_sentence = ''
     syn_sentences = []
     for sentence in sentences:
-        if word_count(last_sentence + ' ' + sentence) > 50:
+        if word_count(last_sentence + ' ' + sentence) > 40 and word_count(last_sentence):
             syn_sentences.append(last_sentence)
             last_sentence = sentence
         else:
