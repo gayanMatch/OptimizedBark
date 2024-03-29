d099cd5bd693823ac8890fff363d90206230ee62
ginger <gayansilva262@gmail.com>
2024-03-29 04:42:19 +0000

    add: log functionality

--- worker.py
+++ worker.py
@@ -2,6 +2,8 @@ import redis
 import json
 import os
 import base64
+from datetime import datetime, timezone
+from pymongo import MongoClient
 import time  # Used for simulating long-running or streaming predictions
 from bark.SynthesizeThread import SynthesizeThread
 from bucket_utils import download_voice
@@ -11,9 +13,18 @@ FLY_MACHINE_ID = os.environ.get("FLY_MACHINE_ID", '1111111111111111111')
 synthesize_thread = SynthesizeThread(DEFAULT_VOICE)
 synthesize_thread.start()
 
-redis_url = os.environ.get("redis_url", 'redis://default:eb7199cbf0f54bf5bb084f7f1d594692@fly-bark-queries.upstash.io:6379')
+redis_url = os.environ.get(
+    "redis_url",
+    'redis://default:eb7199cbf0f54bf5bb084f7f1d594692@fly-bark-queries.upstash.io:6379'
+)
+mongo_uri = os.environ.get(
+    "mongo_uri",
+    "mongodb+srv://ginger:P%40ssw0rd131181@bark-log.1fit2mh.mongodb.net/?retryWrites=true&w=majority&appName=bark-log"
+)
 # Establish connections to Redis for both publishing results and subscribing to incoming tasks
 r = redis.Redis.from_url(redis_url)
+client = MongoClient(mongo_uri)
+collection = client["bark"]["queries"]
 r.setnx('stop_marked_gpu', '')
 # r = redis.Redis(
 #   host='localhost',  # Changed to localhost
@@ -43,37 +54,37 @@ def handle_predictions():
         voice = request["voice"]
         rate = request["rate"]
         request_time = request["request_time"]
-
+        process_start_time = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S.%f")
+        s = time.time()
         if voice + ".npz" not in os.listdir("bark/assets/prompts"):
             download_voice('tts-voices-npz', voice, 'bark/assets/prompts')
         # This is a simplistic approach; consider batching or other optimizations for your actual use case
         stream = synthesize_thread.add_request(text, voice, rate)
 
         # Simulate streaming data; in a real scenario, this loop could be replaced with actual streaming logic
-        # first_byte_time = -1
+        first_byte_time = -1
         for result in stream:
             # Publish the intermediate result to the channel named after 'request_id'
             encoded_result = base64.b64encode(result).decode('utf-8')
-            # if first_byte_time == -1:
-            #     first_byte_time = time.time()
+            if first_byte_time == -1:
+                first_byte_time = time.time() - s
             r.publish(request_id, json.dumps({"prediction": encoded_result}))
-        # finish_time = time.time()
+        finish_time = time.time() - s
         # Signal completion of the streaming predictions
         r.publish(request_id, json.dumps({"complete": True}))
-        # r.lpush(
-        #     "bark-query-logs",
-        #     json.dumps(
-        #         {
-        #             "text": text,
-        #             "voice": voice,
-        #             "rate": rate,
-        #             "request_time": request_time,
-        #             'first_byte_time': first_byte_time,
-        #             'finish_time': finish_time
-        #         }
-        #     )
-        # )
+        collection.insert_one(
+            {
+                "request_time": request_time,
+                "process_start_time": process_start_time,
+                "text": text,
+                "voice": voice,
+                "rate": rate,
+                'first_byte_time': first_byte_time,
+                'finish_time': finish_time
+            }
+        )
         r.incr(f'migs_{FLY_MACHINE_ID}')
+
 
 if __name__ == "__main__":
     print("Starting consumer...")
